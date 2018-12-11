5e784d6a8a47ff1c04abc8e8669a75bfe7d5cb3d
EBcoco <servivalism@163.com>
2018-12-11 14:33:40 +0800

    商品详情

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns=[
-    url(r"^$",views.index,name='index')
+    url(r"^$",views.index,name='index'),
+    url(r"^shop/(\d+)/$",views.shop,name='shop'),
+    url(r"^register/$",views.register,name='register')
 ]
--- app/views.py
+++ app/views.py
@@ -9,3 +9,30 @@ def index(request):
     goods=Goods.objects.all()
 
     return render(request, 'index.html',context={'goods':goods})
+
+
+def shop(request,id):
+    good=Goods.objects.filter(id=id).first()
+    img1=good.img1
+    img2=good.img2
+    img3=good.img3
+    img4=good.img4
+    name=good.name
+    cost=good.cost
+    price=good.price
+    init=good.init
+    data={
+        'img1':img1,
+        'img2':img2,
+        'img3':img3,
+        'img4':img4,
+        'name':name,
+        'price':price,
+        'cost':cost,
+        'init':init
+    }
+    return render(request,'shop.html',context=data)
+
+
+def register(request):
+    return render(request,'register.html')
